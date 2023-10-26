4a7b71bbf6cf2e82df07c907f630be66325171fe
nguyentrunghieu2121051141 <hieunguyen10a11amtruc@gmail.com>
2023-10-26 17:34:32 +0700

    thay đổi thành phần

--- slide35-36/silde36.js
+++ slide35-36/silde36.js
@@ -1,26 +1,16 @@
 function validation(){
-    const password1El = document.getElementById('password1');
-    const password2El = document.getElementById('password2');
-    let passwordsMatch = false;
-
-    if (password1El.value === password2El.value) {
-        passwordsMatch = true;
-        password1El.style.borderColor = 'green';
-        password2El.style.borderColor = 'green';
-      } 
-      else {
-        passwordsMatch = false;
-        message.textContent = 'Mật khẩu không trùng. Vui lòng nhập lại mật khẩu!';
-        message.style.color = 'red';
-        messageContainer.style.borderColor = 'red';
-        password1El.style.borderColor = 'red';
-        password2El.style.borderColor = 'red';
-        return;
-      }
-      
-      if (isValid && passwordsMatch) {
-        message.textContent = 'Đăng kí thành kông!';
-        message.style.color = 'green';
-        messageContainer.style.borderColor = 'green';
-      }
+  let password1 = document.getElementById("password1").value;
+  let password2 = document.getElementById("password2").value;
+  console.log(password1,password2);
+  if(password1.length != 0){
+    if(password1 == password2){
+      alert("Đăng kí thành công!");
+    }
+    else{
+      alert("Mật khẩu không trùng khớp!");
+    }
+  }
+  else{
+    alert("Không được để trống!");
+  }
 }
