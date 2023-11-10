fc829590bb85cdaf5ff8b39c15106c15ba5e2ff1
nguyentrunghieu2121051141 <hieunguyen10a11amtruc@gmail.com>
2023-11-10 10:40:42 +0700

    php

---
+++ php/formGet.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>formGet</title>
+    </head>
+    <body>
+        Welcome <?php echo $_GET["name"]; ?><br>
+        Your email address is: <?php echo $_GET["email"]; ?> 
+    </body>
+</html>
