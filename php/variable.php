ddee291acc13f352321c58f24302f776ff4b126b
nguyentrunghieu2121051141 <hieunguyen10a11amtruc@gmail.com>
2023-11-10 09:22:52 +0700

    php

---
+++ php/variable.php
@@ -0,0 +1,8 @@
+<?php
+    $subject = "Lap trinh ung dung web";
+    $universityName = "HUMG";
+    $firstNumber = 2;
+    $secondNumber = 2;
+
+    echo "Hoc". $subject. "o phong 502 tai truong". $universityName. "<br>";
+?>
