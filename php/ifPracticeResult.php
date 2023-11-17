bab8590d86c34c31673a049dc0a87c49658395f7
nguyentrunghieu2121051141 <hieunguyen10a11amtruc@gmail.com>
2023-11-17 10:21:09 +0700

    php

--- php/ifPracticeResult.php
+++ php/ifPracticeResult.php
@@ -4,18 +4,22 @@
     $JPY = 200;
     $EUR = 27000;
     $amount = $_POST["amount"];
-    echo"$amount USD is equal";
+    echo"$amount VNĐ đổi được ";
     if($_POST["currency"] == "USD"){
-        echo $amount * $USD;
+        echo $amount * $USD ;
+        echo" USD";
     }
     elseif($_POST["currency"] == "EUR"){
         echo $amount * $EUR;
+        echo" EUR";
     }
     elseif($_POST["currency"] == "JPY"){
         echo $amount * $JPY;
+        echo" JPY";
     }
     else{
         echo $amount * $AUD;
+        echo" AUD";
     }
-    echo"VND";
+    
 ?>
